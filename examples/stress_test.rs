1878036274e2c89d3aee4557bde05c2c755e99b3
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2025-12-22 14:37:10 +0100

    chore: despawn should be fine (has to exist)

--- examples/stress_test.rs
+++ examples/stress_test.rs
@@ -82,7 +82,7 @@ fn setup(
     config: ResMut<StressTestConfig>,
     existing: Query<Entity, With<InstanceMaterialData>>,
 ) {
-    existing.iter().for_each(|e| cmd.entity(e).try_despawn());
+    existing.iter().for_each(|e| cmd.entity(e).despawn());
     meshes.ids().collect::<Vec<_>>().into_iter().for_each(|x| {
         meshes.remove(x);
     });
